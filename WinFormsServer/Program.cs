22e2ab0c8b312f2c59005583b1f25d9966ea9172
danieleli <danschlossberg@gmail.com>
2017-07-14 07:58:09 -0700

    'touch'

--- WinFormsServer/Program.cs
+++ WinFormsServer/Program.cs
@@ -14,6 +14,7 @@ namespace SignalRChat
         static void Main()
         {
             Application.EnableVisualStyles();
+
             Application.SetCompatibleTextRenderingDefault(false);
             MainForm = new WinFormsServer();
             Application.Run(MainForm);
