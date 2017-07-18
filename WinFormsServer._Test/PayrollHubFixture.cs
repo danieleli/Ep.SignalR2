19f29b1ebfadeb542d3b180e6ae892b5f6aec054
danieleli <danschossberg@gmail.com>
2017-07-18 13:17:28 -0700

    more log4net config

--- WinFormsServer._Test/PayrollHubFixture.cs
+++ WinFormsServer._Test/PayrollHubFixture.cs
@@ -26,14 +26,32 @@ namespace WinFormsServer._Test
             var conn = new HubConnection(SERVER_URI);
             var proxy = conn.CreateHubProxy(HUB_NAME);
             var eventName = "BatchUpdated";
-            var eventFired = false;
+            var eventHeard = false;
             
             proxy.On<int, string>(eventName, (id, status) =>
             {
                 _log.Debug($"Proxy.On listner delegate. EventName: {eventName}, ID: {id}, Status: {status}");
-                eventFired = true;
+                eventHeard = true;
             });
            
+            StartConnection(conn);
+
+            _log.Debug($"proxy.Invoke({HUB_NAME}, 1, somestatus)");
+            proxy.Invoke(eventName, 1, "somestatus");
+
+            for (int i = 0; i < 20; i++)
+            {
+                Thread.Sleep(10);
+                Console.WriteLine($"waiting to hear event. {i}");
+                if (eventHeard) return;
+            }
+
+            Assert.Fail("Event not fired");
+            
+        }
+
+        private static void StartConnection(HubConnection conn)
+        {
             var task = conn.Start();
             var counter = 0;
             while (!task.IsCompleted)
@@ -42,19 +60,6 @@ namespace WinFormsServer._Test
                 Thread.Sleep(100);
                 _log.Debug($"waiting for connection.start...{counter}");
             }
-            
-            _log.Debug($"proxy.Invoke({HUB_NAME}, 1, somestatus)");
-            proxy.Invoke(eventName, 1, "somestatus");
-
-            for (int i = 0; i < 20; i++)
-            {
-                Thread.Sleep(10);
-                Console.WriteLine($"wait {i}");
-                if (eventFired) return;
-            }
-
-            Assert.Fail("Event not fired");
-            
         }
 
         private static readonly ILog _log = LogManager.GetLogger(typeof(PayrollHubFixture));
